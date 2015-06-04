4bd395fb9b462fc8a7257a35c9ffe71095add4aa
Erik de Jonge <erik@a8.nl>
2015-06-04 15:10:58 +0200

    Bob Marley: Money cant buy life. Thursday 04 June 2015 (week:22 day:155), 15:10:58

--- mdcodeblockcorrect.py
+++ mdcodeblockcorrect.py
@@ -13,10 +13,11 @@ Options:
 """
 from arguments import Arguments
 from consoleprinter import forceascii
+
 import os
 
 
-def correct_codeblocks(mdfile, force=False):
+def correct_codeblocks(mdfile, force=False, fromsrt=False):
     """
     @type mdfile: str
     @return: None
@@ -25,6 +26,7 @@ def correct_codeblocks(mdfile, force=False):
         inbuf = [x.rstrip().replace("\t", "    ") for x in open(mdfile)]
     except:
         inbuf = []
+
         for l in open(mdfile):
             inbuf.append(forceascii(l).replace("\t", "    "))
 
@@ -39,50 +41,50 @@ def correct_codeblocks(mdfile, force=False):
                 return 0
 
     for l in inbuf:
+        if fromsrt:
+            l = l.replace("###", "")
 
-        l = l.replace("###", "")
-        if l.strip().startswith("`") and l.strip().endswith("`") and "```" not in l:
-            l = l.replace("`", "##", 1).lstrip()
-            l = l.replace("`", "")
-        if "`" in l and l.count("`")!=3:
-            l = l.replace("`", "")
-        if l.strip().startswith("```"):
-            if inblock:
-                inblock = False
-            else:
-                inblock = True
+            if l.strip().startswith("`") and l.strip().endswith("`") and "```" not in l:
+                l = l.replace("`", "##", 1).lstrip()
+                l = l.replace("`", "")
 
-        if not inblock:
-            if (l.strip().startswith("sed") or l.strip().startswith("gsed")) and not cb:
-                outbuf.append("\n```bash")
-                cnt += 1
-                cb = True
+            if "`" in l and l.count("`") != 3:
+                l = l.replace("`", "")
 
-            elif l.startswith("    ") and not l.strip().startswith("<") and not "/>" in l and not l.endswith(";") and not "`" in l and not cb :
-                if not cb:
-                    cnt += 1
-                    outbuf.append("\n```python")
+            if l.strip().startswith("```"):
+                if inblock:
+                    inblock = False
+                else:
+                    inblock = True
 
-                cb = True
-            else:
+            if not inblock:
+                if not l.strip().startswith("-") and not l.strip().startswith("1") and not l.strip().startswith(">"):
+                    if (l.strip().startswith("sed") or l.strip().startswith("gsed")) and not cb:
+                        outbuf.append("\n```bash")
+                        cnt += 1
+                        cb = True
+                    elif l.startswith("    ") and not l.strip().startswith("<") and not "/>" in l and not l.endswith(";") and not "`" in l and not cb:
+                        if not cb:
+                            cnt += 1
+                            outbuf.append("\n```python")
+
+                        cb = True
+                    else:
+                        if cb is True:
+                            if not (l.strip().startswith("sed") or l.strip().startswith("gsed") or l.startswith("    ") or len(l.strip()) == 0):
+                                cb = False
+                                outbuf.append("```\n")
+
                 if cb is True:
-                    if not (l.strip().startswith("sed") or l.strip().startswith("gsed") or l.startswith("    ") or len(l.strip())==0):
-                        cb = False
-                        outbuf.append("```\n")
+                    l = l.replace("    ", "", 1)
 
-            if cb is True:
-                l = l.replace("    ", "", 1)
-        # used for sed cheatcheat conversion
-        # if l.endswith(":"):
-        #     l = "#"+l.lower().capitalize()
-        # else:
-        #     if l.strip().startswith("# "):
-        #         l = l.strip().replace("# ", "").capitalize()
         outbuf.append(l)
 
     if cb is True:
         outbuf.append("```")
+
     outbuf = "\n".join(outbuf)
+
     if force is True:
         outbuf = outbuf.replace("```", "\n```")
         outbuf = outbuf.replace("\n\n\n```", "\n\n```")
@@ -92,7 +94,7 @@ def correct_codeblocks(mdfile, force=False):
         outbuf = outbuf.replace("-   [", "- [")
         outbuf = outbuf.replace("-   ", "- ")
         outbuf = outbuf.replace("1.   ", "1. ")
-
+        outbuf = outbuf.replace("\n\n\n", "\n\n")
     open(mdfile, "w").write(outbuf)
     return cnt
 
@@ -102,21 +104,17 @@ def main():
     main
     """
     arg = Arguments(doc=__doc__)
-
     if arg.verbose is True:
         print(arg)
-
     if arg.mdfile.lower().strip().endswith(".markdown"):
-        print("mv "+arg.mdfile+" "+arg.mdfile.replace(".markdown", ".md"))
-        os.system("mv "+arg.mdfile+" "+arg.mdfile.replace(".markdown", ".md"))
-
+        print("mv " + arg.mdfile + " " + arg.mdfile.replace(".markdown", ".md"))
+        os.system("mv " + arg.mdfile + " " + arg.mdfile.replace(".markdown", ".md"))
     if not os.path.exists(arg.mdfile):
         print("file does not exist")
         return
 
     mdfile = arg.mdfile
     cnt = correct_codeblocks(mdfile, arg.force)
-
     if not arg.silent:
         if cnt != 0:
             print("\033[34m" + arg.mdfile.lower(), "->\033[0;96m " + str(cnt) + " code blocks corrected\033[0m")
