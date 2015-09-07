9afb308e9a893b3cabd7efd29bcbfd4c6c22ceca
Erik de Jonge <erik@a8.nl>
2015-09-07 16:49:52 +0200

    Monday 07 September 2015 (week:36 day:250), 16:49:49

--- get_readmes.py
+++ get_readmes.py
@@ -20,50 +20,37 @@ import os
 import arguments
 
 
-
 def check_folder(bs):
     """
     @type bs: list
     @return: None
     """
-
     for d in os.listdir(bs):
-
-
         fp = os.path.join(bs, d)
         rm = os.path.join(fp, "readme.md")
-        np = "githubreadme" + "/"+ d + "/readme.md"
+        np = "./githubreadme" + "/" + d + "/readme.md"
 
         if os.path.exists(rm):
-            print(rm, np)
             c = open(rm).read()
-
-
+            print(os.path.abspath(os.path.dirname(np)), np, len(c))
             os.makedirs(os.path.dirname(np), exist_ok=True)
             open(np, "w").write(c)
             pass
 
-
-
-        #if os.path.isdir(fp):
-        ##    print("\033[95m>", fp ,"\033[0m")
-        #    check_folder(fp)
 
 def main():
     """
     main
     """
     arg = arguments.Arguments(__doc__)
-
     targetdir = os.path.expanduser(arg.target_dir)
+
     if targetdir == ".":
         targetdir = os.getcwd()
+
     outdir = os.getcwd()
-    os.makedirs(os.path.join(outdir, "githubreadme"), exist_ok=True)
-    os.chdir(os.path.join(outdir, "githubreadme"))
 
-    os.system("rm -Rf "+os.path.join(outdir, "githubreadme")+"/*")
-
+    #os.system("rm -Rf "+os.path.join(outdir, "githubreadme")+"/*")
     targetdir = os.path.expanduser(targetdir)
     os.chdir(targetdir)
     check_folder(targetdir)
