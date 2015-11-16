f27d99fad0cfa993c21708c8f1a6d59993bcde66
Erik de Jonge <erik@a8.nl>
2015-11-16 11:11:11 +0100

    Monday 16 November 2015 (week:46 day:320), 11:11:04

--- mdcodeblockcorrect.py
+++ mdcodeblockcorrect.py
@@ -10,6 +10,7 @@ Options:
   -v --verbose  Print arguments
   -f --force    Force check
   -s --silent   Folder from where to run the command [default: .].
+  -p --forpdf      Optimize for pdf
 """
 from arguments import Arguments
 from consoleprinter import forceascii
@@ -17,11 +18,12 @@ from consoleprinter import forceascii
 import os
 
 
-def correct_codeblocks(mdfile, force=False, fromsrt=False):
+def correct_codeblocks(mdfile, force=False, fromsrt=False, forpdf=False):
     """
     @type mdfile: str
     @return: None
     """
+
     ismd = mdfile.strip().endswith("md")
     if force is True:
         if fromsrt is False:
@@ -56,7 +58,6 @@ def correct_codeblocks(mdfile, force=False, fromsrt=False):
                     inblock = False
                 else:
                     inblock = True
-
             if not inblock:
                 if l.strip().startswith("--"):
                     optionsblock = True
@@ -67,10 +68,14 @@ def correct_codeblocks(mdfile, force=False, fromsrt=False):
                         outbuf.append("\n```bash")
                         cnt += 1
                         cb = True
-                    elif (l.startswith("    ") or l.startswith("\t"))and not l.strip().startswith("<") and not "/>" in l and not l.endswith(";") and not "`" in l and not cb:
+                    elif (l.startswith("    ") or l.startswith("\t")) and not l.strip().startswith("<!") and not "/>" in l and not l.endswith(";") and not "`" in l and not cb:
                         cnt += 1
                         if l.strip().startswith("$") or 'brew' in l or 'sudo' in l or 'pip' in l:
                             outbuf.append("\n``` bash")
+                        elif l.strip().startswith("SSL") or 'Apache' in l or 'mod_' in l or "Header append" in l:
+                            outbuf.append("\n``` apacheconf")
+                        elif '<html>' in l or '<p>' in  l or '&lt;html' in l or '&lt;p' in l:
+                            outbuf.append("\n``` html")
                         else:
                             outbuf.append("\n``` python")
 
@@ -94,6 +99,7 @@ def correct_codeblocks(mdfile, force=False, fromsrt=False):
     if force is True:
         outbuf = outbuf.replace("```", "\n```")
         outbuf = outbuf.replace("\n\n\n```", "\n\n```")
+
         outbuf = outbuf.replace("```\n\n```", "")
         outbuf = outbuf.replace("```\n\n python\n", "\n\n``` python\n")
         outbuf = outbuf.replace("```\n\n bash\n", "\n\n``` bash\n")
@@ -103,11 +109,35 @@ def correct_codeblocks(mdfile, force=False, fromsrt=False):
         outbuf = outbuf.replace("[¶]", "[]")
         outbuf = outbuf.replace("\[", "[")
         outbuf = outbuf.replace("\]", "]")
+        outbuf = outbuf.replace("```", "@#@#@#")
+        outbuf = outbuf.replace(" `", " ***")
+        outbuf = outbuf.replace("` ", "*** ")
+        outbuf = outbuf.replace("@#@#@#", "```")
         outbuf = outbuf.replace("\n\n\n", "\n\n")
         outbuf = outbuf.replace("programlisting", "python")
         outbuf = outbuf.replace("![](2.%20Why%20Value%20Matters%20Less%20with%20Competition.resources/C8D7D470-141C-4985-B463-A7C355237157.jpg)", "- ")
 
-    open(mdfile, "w").write(outbuf)
+    inblock = False
+    outbuf2= ""
+    colwidth = 60
+    for l in outbuf.split('\n'):
+        if inblock and forpdf is True:
+
+            if len(l) > colwidth:
+                index = l[colwidth:].find(' ')
+                if index > 0:
+                    index = index + colwidth
+                    l = l[:index]+'\n\t'+l[index:]
+
+        if inblock and '```' in l:
+            outbuf2 += l
+        else:
+            outbuf2 += '\n' +l
+
+        if '```' in l:
+            inblock = not inblock
+
+    open(mdfile, "w").write(outbuf2)
     return cnt
 
 
@@ -126,7 +156,8 @@ def main():
         return
 
     mdfile = arg.mdfile
-    cnt = correct_codeblocks(mdfile, arg.force)
+    forpdf = arg.forpdf
+    cnt = correct_codeblocks(mdfile, arg.force, forpdf=forpdf)
     if not arg.silent:
         if cnt != 0:
             print("\033[34m" + arg.mdfile.lower(), "->\033[0;96m " + str(cnt) + " code blocks corrected\033[0m")
